a9102f2bd262ccef5eb1dbca2b7dcf75c4188d96
Pavel <pavel.r@fora-soft.com>
2025-06-24 19:03:01 +0300

    add: change error's color; add margin after error

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,49 +1,49 @@
-import '@/i18n';
-import { DarkTheme, DefaultTheme } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import 'react-native-reanimated';
+// import '@/i18n';
+import { DarkTheme, DefaultTheme } from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import "react-native-reanimated";
 
-import { AppProvider } from '@/components/appProvider/AppProvider';
-import { useColorScheme } from '@/hooks/useColorScheme';
-import { SplashScreenController } from '@/components/splash/SplashScreenController';
-import { useSession } from '@/components/appProvider/session/SessionContext';
+import { AppProvider } from "@/components/appProvider/AppProvider";
+import { useColorScheme } from "@/hooks/useColorScheme";
+import { SplashScreenController } from "@/components/splash/SplashScreenController";
+import { useSession } from "@/components/appProvider/session/SessionContext";
 
 export default function RootLayout() {
-	const colorScheme = useColorScheme();
-	const [loaded] = useFonts({
-		MontserratRegular: require('../assets/fonts/Montserrat-Regular.ttf'),
-		MontserratMedium: require('../assets/fonts/Montserrat-Medium.ttf'),
-		MontserratSemiBold: require('../assets/fonts/Montserrat-SemiBold.ttf'),
-		MontserratBold: require('../assets/fonts/Montserrat-Bold.ttf'),
-	});
+  const colorScheme = useColorScheme();
+  const [loaded] = useFonts({
+    MontserratRegular: require("../assets/fonts/Montserrat-Regular.ttf"),
+    MontserratMedium: require("../assets/fonts/Montserrat-Medium.ttf"),
+    MontserratSemiBold: require("../assets/fonts/Montserrat-SemiBold.ttf"),
+    MontserratBold: require("../assets/fonts/Montserrat-Bold.ttf"),
+  });
 
-	if (!loaded) {
-		// Async font loading only occurs in development.
-		return null;
-	}
+  if (!loaded) {
+    // Async font loading only occurs in development.
+    return null;
+  }
 
-	return (
-		<AppProvider theme={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-			<SplashScreenController />
-			<RootNavigator />
-		</AppProvider>
-	);
+  return (
+    <AppProvider theme={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+      <SplashScreenController />
+      <RootNavigator />
+    </AppProvider>
+  );
 }
 
 function RootNavigator() {
-	const { session } = useSession();
-	const isEmptySession = Object.keys(session ?? {}).length === 0;
+  const { session } = useSession();
+  const isEmptySession = Object.keys(session ?? {}).length === 0;
 
-	return (
-		<Stack>
-			<Stack.Protected guard={!isEmptySession}>
-				<Stack.Screen name='(tabs)' options={{ headerShown: false }} />
-			</Stack.Protected>
+  return (
+    <Stack>
+      <Stack.Protected guard={!isEmptySession}>
+        <Stack.Screen name='(tabs)' options={{ headerShown: false }} />
+      </Stack.Protected>
 
-			<Stack.Protected guard={isEmptySession}>
-				<Stack.Screen name='sign-in' options={{ headerShown: false }} />
-			</Stack.Protected>
-		</Stack>
-	);
+      <Stack.Protected guard={isEmptySession}>
+        <Stack.Screen name='sign-in' options={{ headerShown: false }} />
+      </Stack.Protected>
+    </Stack>
+  );
 }
--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,156 +1,162 @@
-import LogoIcon from '@/assets/icons/logo-icon.svg';
-import { useSession } from '@/components/appProvider/session/SessionContext';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedViewWithSafeArea } from '@/components/ThemedViewWithSafeArea';
-import { Button } from '@/components/ui/Button';
-import { Input } from '@/components/ui/Input';
-import { useLogin } from '@/services/auth';
-import { yupResolver } from '@hookform/resolvers/yup';
-import { LinearGradient } from 'expo-linear-gradient';
-import { createRef } from 'react';
-import { Controller, useForm } from 'react-hook-form';
+import LogoIcon from "@/assets/icons/logo-icon.svg";
+import { useSession } from "@/components/appProvider/session/SessionContext";
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedViewWithSafeArea } from "@/components/ThemedViewWithSafeArea";
+import { Button } from "@/components/ui/Button";
+import { Input } from "@/components/ui/Input";
+import { useLogin } from "@/services/auth";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { LinearGradient } from "expo-linear-gradient";
+import { createRef } from "react";
+import { Controller, useForm } from "react-hook-form";
 import {
-	Keyboard,
-	StyleSheet,
-	TouchableWithoutFeedback,
-	View,
-} from 'react-native';
-import * as yup from 'yup';
+  Keyboard,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  View,
+} from "react-native";
+import * as yup from "yup";
 
 const schema = yup
-	.object({
-		email: yup.string().email().required(),
-		password: yup.string().min(8).required(),
-	})
-	.required();
+  .object({
+    email: yup
+      .string()
+      .email("Please enter valid email.")
+      .required("Please enter valid email."),
+    password: yup
+      .string()
+      .min(8, "Please enter your password.")
+      .required("Please enter your password."),
+  })
+  .required();
 
 export default function LoginScreen() {
-	const inputRef = createRef<any>();
-	const {
-		control,
-		handleSubmit,
-		formState: { errors },
-	} = useForm({
-		resolver: yupResolver(schema),
-	});
+  const inputRef = createRef<any>();
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
 
-	const { signIn } = useSession();
+  const { signIn } = useSession();
 
-	const { mutate: login } = useLogin();
+  const { mutate: login } = useLogin();
 
-	const onSubmit = (data: any) => {
-		login(data, {
-			onSuccess: res => {
-				signIn(res.data.success.user);
-			},
-			onError: err => {
-				console.error('Login error', err);
-				console.log('err.message', err.message);
-				console.log('err.response', err.response);
-				console.log('err.response.status', err.response.status);
-			},
-		});
-	};
+  const onSubmit = (data: any) => {
+    login(data, {
+      onSuccess: res => {
+        signIn(res.data.success.user);
+      },
+      onError: err => {
+        console.error("Login error", err);
+        console.log("err.message", err.message);
+        console.log("err.response", err.response);
+        console.log("err.response.status", err.response.status);
+      },
+    });
+  };
 
-	// if (inputRef.current) {
-	// 	inputRef.current.shake();
-	// }
-	return (
-		<ThemedViewWithSafeArea
-			safeEdges={['top', 'right', 'bottom', 'left']}
-			style={styles.container}
-		>
-			<TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-				<View style={styles.content}>
-					<View style={styles.logoContainer}>
-						<LogoIcon />
-						<ThemedText style={styles.title} type='displayXs'>
-							Welcome back to&nbsp;FoxRunner
-						</ThemedText>
-					</View>
-					<View style={styles.formContainer}>
-						<View>
-							<Controller
-								name='email'
-								control={control}
-								render={({ field }) => (
-									<Input
-										type='text'
-										label='Email'
-										placeholder='Enter your email'
-										value={field.value}
-										onChangeText={field.onChange}
-										onBlur={field.onBlur}
-										errorMessage={errors.email?.message}
-									/>
-								)}
-							/>
-							<Controller
-								name='password'
-								control={control}
-								render={({ field }) => (
-									<Input
-										type='password'
-										label='Password'
-										placeholder='Enter your password'
-										value={field.value}
-										onChangeText={field.onChange}
-										onBlur={field.onBlur}
-										ref={inputRef}
-										rightIconContainerStyle={styles.iconContainerExtra}
-										errorMessage={errors.password?.message}
-									/>
-								)}
-							/>
-						</View>
-						<Button
-							ViewComponent={LinearGradient as any}
-							linearGradientProps={{
-								colors: ['#FF692E', '#FF4405'],
-								start: [0, 0],
-								end: [1, 0],
-							}}
-							onPress={handleSubmit(onSubmit)}
-							title='Log in'
-						/>
-					</View>
-				</View>
-			</TouchableWithoutFeedback>
-		</ThemedViewWithSafeArea>
-	);
+  // if (inputRef.current) {
+  // 	inputRef.current.shake();
+  // }
+  return (
+    <ThemedViewWithSafeArea
+      safeEdges={["top", "right", "bottom", "left"]}
+      style={styles.container}
+    >
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style={styles.content}>
+          <View style={styles.logoContainer}>
+            <LogoIcon />
+            <ThemedText style={styles.title} type='displayXs'>
+              Welcome back to&nbsp;FoxRunner
+            </ThemedText>
+          </View>
+          <View style={styles.formContainer}>
+            <View>
+              <Controller
+                name='email'
+                control={control}
+                render={({ field }) => (
+                  <Input
+                    type='text'
+                    label='Email'
+                    placeholder='Enter your email'
+                    value={field.value}
+                    onChangeText={field.onChange}
+                    onBlur={field.onBlur}
+                    errorMessage={errors.email?.message}
+                  />
+                )}
+              />
+              <Controller
+                name='password'
+                control={control}
+                render={({ field }) => (
+                  <Input
+                    type='password'
+                    label='Password'
+                    placeholder='Enter your password'
+                    value={field.value}
+                    onChangeText={field.onChange}
+                    onBlur={field.onBlur}
+                    ref={inputRef}
+                    rightIconContainerStyle={styles.iconContainerExtra}
+                    errorMessage={errors.password?.message}
+                  />
+                )}
+              />
+            </View>
+            <Button
+              ViewComponent={LinearGradient as any}
+              linearGradientProps={{
+                colors: ["#FF692E", "#FF4405"],
+                start: [0, 0],
+                end: [1, 0],
+              }}
+              onPress={handleSubmit(onSubmit)}
+              title='Log in'
+            />
+          </View>
+        </View>
+      </TouchableWithoutFeedback>
+    </ThemedViewWithSafeArea>
+  );
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-	},
+  container: {
+    flex: 1,
+  },
 
-	content: {
-		flex: 1,
-		paddingTop: 32,
-		paddingBottom: 16,
-		paddingHorizontal: 16,
-	},
+  content: {
+    flex: 1,
+    paddingTop: 32,
+    paddingBottom: 16,
+    paddingHorizontal: 16,
+  },
 
-	logoContainer: {
-		alignItems: 'center',
-		marginTop: 16,
-	},
+  logoContainer: {
+    alignItems: "center",
+    marginTop: 16,
+  },
 
-	title: {
-		width: 286,
-		textAlign: 'center',
-		fontWeight: 700,
-		fontFamily: 'MontserratBold',
-	},
+  title: {
+    width: 286,
+    textAlign: "center",
+    fontWeight: 700,
+    fontFamily: "MontserratBold",
+  },
 
-	formContainer: {
-		flex: 1,
-		justifyContent: 'space-between',
-		marginTop: 32,
-	},
+  formContainer: {
+    flex: 1,
+    justifyContent: "space-between",
+    marginTop: 32,
+  },
 
-	iconContainerExtra: {
-		marginRight: 11,
-	},
+  iconContainerExtra: {
+    marginRight: 11,
+  },
 });
