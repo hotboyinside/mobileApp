22caa1ca6207b54ab79b48a860406dbbcb74df23
pavel <pavel.r@fora-soft.com>
2025-10-17 20:08:07 +0300

    test: add new page Shop with app-in-app-purchases for testing on IOS device

---
+++ app/shop.tsx
@@ -0,0 +1,5 @@
+import { Shop } from '@/components/pages/shop/Shop';
+
+export default function ShopScreen() {
+	return <Shop />;
+}
--- constants/routes.ts
+++ constants/routes.ts
@@ -1,12 +1,13 @@
-import { urlJoin } from "@/helpers/route";
-import { RelativePathString } from "expo-router";
+import { urlJoin } from '@/helpers/route';
+import { RelativePathString } from 'expo-router';
 
-export const ALL_NEWS = "(tabs)" as RelativePathString;
+export const ALL_NEWS = '(tabs)' as RelativePathString;
 
-export const NEWS_LIST = "/news";
+export const NEWS_LIST = '/news';
 export const NEWS_DETAILS = (id: string) =>
-  urlJoin(NEWS_LIST, id) as RelativePathString;
+	urlJoin(NEWS_LIST, id) as RelativePathString;
 
-export const NOTIFICATIONS = "/notifications" as RelativePathString;
-export const THEME = "/theme" as RelativePathString;
-export const MODAL = "/modal" as RelativePathString;
+export const NOTIFICATIONS = '/notifications' as RelativePathString;
+export const SHOP = '/shop' as RelativePathString;
+export const THEME = '/theme' as RelativePathString;
+export const MODAL = '/modal' as RelativePathString;
